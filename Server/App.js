79437d6d376db89173bf1f47262bb27b02445a0d
GURDEEP-SINGH01 <gurdeepsingh1431999@gmail.com>
2024-03-25 00:01:22 +0530

    removed the model

--- Server/App.js
+++ Server/App.js
@@ -9,18 +9,8 @@ const productRoutes = require("./Routes/productRoutes");
 const app = express();
 
 initializeFirebaseApp();
-const mongoose = require("mongoose");
-const Usermodel = require("./Model/UserSchema");
-const Productsmodel = require("./Model/ProductsSchema");
-mongoose.connect("mongodb+srv://gurdeep:mongodb@cluster0.d9xlx6g.mongodb.net/");
 
-const db = mongoose.connection;
-db.on("error", (err) => {
-  console.log("Error occured while connecting " + err);
-});
-db.once("open", () => {
-  console.log("Connection made with Db");
-});
+
 
 app.use(
   cors({
@@ -43,17 +33,6 @@ app.use(
 
 app.use("/user", UserRoutes);
 app.use("/products", productRoutes);
-app.get("/user/get/:id", async (req, res) => {
-  try {
-    const username = req.params.id;
-    console.log(username);
-    const user =await Productsmodel.find({id:username});
-    console.log(user);
-    res.send(user);
-  } catch (err) {
-    res.send("Error" + err);
-  }
-});
 
 app.listen(process.env.PORT || 4000, () => {
   console.log(`server running on port ${process.env.PORT || 4000}`);
